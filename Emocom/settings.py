4b2ddfe72634aa9de4ec459fdb09d44d5311d7c6
AmosMaru <amosmaru10@gmail.com>
2024-03-14 19:16:27 +0300

    change

--- Emocom/settings.py
+++ Emocom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v!t+@!+%mn_vvya!!%m)286-t0!#ek(=o^ggma@d_1*=157l$1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
