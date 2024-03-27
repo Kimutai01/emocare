4ca9a8933cb8387c74f1263bce4c1713070c6cba
AmosMaru <amosmaru10@gmail.com>
2024-03-27 18:02:11 +0300

    push

--- emotions/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . models import Profile, Emotion, Plans,EmailHist
-
-admin.site.register(Emotion)
-admin.site.register(Profile)
-admin.site.register(Plans)
-admin.site.register(EmailHist)
